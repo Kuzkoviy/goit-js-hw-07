9ee0a295777394454682722d746d09e771f94caa
Kuzkoviy <72725653+Kuzkoviy@users.noreply.github.com>
2024-04-24 23:48:26 +0300

    task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector('.login-form');
 
+ const onDataSubmit = event => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email.trim() === '' || password.trim() === '') {
+        form.elements.email.value = '';
+        form.elements.password.value = '';
+        return alert('All form fields must be filled in');
+    }
+
+    const objInfo = {
+        email: email.trim(),
+        password: password.trim(),
+    }
+
+    console.log(objInfo);
+    form.reset();
+};
+
+loginForm.addEventListener('submit', onDataSubmit);
