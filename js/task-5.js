24dc35938ab67896a397d57e5f9eba060aa8bd48
Kuzkoviy <72725653+Kuzkoviy@users.noreply.github.com>
2024-04-24 23:53:27 +0300

    task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const color = document.querySelector('.color');
+
+const getColorOfBody = () => {
+  const backgroundColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = backgroundColor;
+  color.textContent = backgroundColor;
+}
+
+const changeColor = document.querySelector('.change-color');
+changeColor.addEventListener('click', getColorOfBody);
